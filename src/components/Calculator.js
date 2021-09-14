9aa218223ebc116a879a80b3d4fcc44bcb2742b4
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-14 13:07:01 +0200

    Fix indentation in Calclulator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,26 +1,26 @@
 const Calculator = () => (
   <div className="calculator">
-      <div className="display">
-        <span className="displayNumber">0</span>
-      </div>
-      <div className="keyboard">
-        <button type="button" className="key" id="AC">AC</button>
-        <button type="button" className="key " id="+/-">+/-</button>
-        <button type="button" className="key" id="%">%</button>
-        <button type="button" className="key orange-key" id="÷">÷</button>
-        <button type="button" className="key" id="7">7</button>
-        <button type="button" className="key" id="8">8</button>
-        <button type="button" className="key" id="9">9</button>
-        <button type="button" className="key orange-key" id="x">X</button>
-        <button type="button" className="key" id="4">4</button>
-        <button type="button" className="key" id="5">5</button>
-        <button type="button" className="key" id="6">6</button>
-        <button type="button" className="key orange-key" id="-">-</button>
-        <button type="button" className="key big-key" id="0">0</button>
-        <button type="button" className="key" id=".">.</button>
-        <button type="button" className="key orange-key" id="=">=</button>
-      </div>
+    <div className="display">
+      <span className="displayNumber">0</span>
     </div>
+    <div className="keyboard">
+      <button type="button" className="key" id="AC">AC</button>
+      <button type="button" className="key " id="+/-">+/-</button>
+      <button type="button" className="key" id="%">%</button>
+      <button type="button" className="key orange-key" id="÷">÷</button>
+      <button type="button" className="key" id="7">7</button>
+      <button type="button" className="key" id="8">8</button>
+      <button type="button" className="key" id="9">9</button>
+      <button type="button" className="key orange-key" id="x">X</button>
+      <button type="button" className="key" id="4">4</button>
+      <button type="button" className="key" id="5">5</button>
+      <button type="button" className="key" id="6">6</button>
+      <button type="button" className="key orange-key" id="-">-</button>
+      <button type="button" className="key big-key" id="0">0</button>
+      <button type="button" className="key" id=".">.</button>
+      <button type="button" className="key orange-key" id="=">=</button>
+    </div>
+  </div>
 );
 
 export default Calculator;
