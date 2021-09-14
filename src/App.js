d03a348c0579db77a6382edc1cdb9a6a057a2f39
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-14 12:40:01 +0200

    Render App and Calculator component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+import './App.css';
+// eslint-disable-next-line no-unused-vars
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
