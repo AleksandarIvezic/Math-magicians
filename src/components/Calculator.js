550134e6c5a13f200d9ff087cc37c4d04faedb83
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-14 12:58:11 +0200

    Fix missing button type linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,21 +4,21 @@ const Calculator = () => (
         <span className="displayNumber">0</span>
       </div>
       <div className="keyboard">
-        <button className="key" id="AC">AC</button>
-        <button className="key " id="+/-">+/-</button>
-        <button className="key" id="%">%</button>
-        <button className="key orange-key" id="÷">÷</button>
-        <button className="key" id="7">7</button>
-        <button className="key" id="8">8</button>
-        <button className="key" id="9">9</button>
-        <button className="key orange-key" id="x">X</button>
-        <button className="key" id="4">4</button>
-        <button className="key" id="5">5</button>
-        <button className="key" id="6">6</button>
-        <button className="key orange-key" id="-">-</button>
-        <button className="key big-key" id="0">0</button>
-        <button className="key" id=".">.</button>
-        <button className="key orange-key" id="=">=</button>
+        <button type="button" className="key" id="AC">AC</button>
+        <button type="button" className="key " id="+/-">+/-</button>
+        <button type="button" className="key" id="%">%</button>
+        <button type="button" className="key orange-key" id="÷">÷</button>
+        <button type="button" className="key" id="7">7</button>
+        <button type="button" className="key" id="8">8</button>
+        <button type="button" className="key" id="9">9</button>
+        <button type="button" className="key orange-key" id="x">X</button>
+        <button type="button" className="key" id="4">4</button>
+        <button type="button" className="key" id="5">5</button>
+        <button type="button" className="key" id="6">6</button>
+        <button type="button" className="key orange-key" id="-">-</button>
+        <button type="button" className="key big-key" id="0">0</button>
+        <button type="button" className="key" id=".">.</button>
+        <button type="button" className="key orange-key" id="=">=</button>
       </div>
     </div>
 );
