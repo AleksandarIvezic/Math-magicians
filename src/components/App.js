51bf38fe0c80001c2d1ce979be4d8b33aa3ebbbe
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-21 15:06:01 +0200

    Apply Route and Switch in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './Calculator';
+import Navbar from './Navbar';
+import Quote from './Quote';
+import Home from './Home';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,9 +16,20 @@ class App extends React.Component {
   render() {
     const { startValue } = this.state;
     return (
-      <div className="App">
-        <Calculator start={startValue} />
-      </div>
+      <>
+        <Navbar />
+        <Switch>
+          <Route path="/calculator">
+            <Calculator start={startValue} />
+          </Route>
+          <Route path="/quote">
+            <Quote />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </>
     );
   }
 }
