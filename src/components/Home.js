69e850f00847aa3da05f9f67b33ae410e71d3718
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-21 17:33:12 +0200

    Refactor Quote.js to get data from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 /* eslint-disable*/
+import React from 'react';
 import '../style/Home.css';
 
 const Home = () => (
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,3 +1,24 @@
-const Quote = () => <h1>Quotepage</h1>;
+import React, { useState, useEffect } from 'react';
+import getQuote from '../helpers/getQuote';
+
+const Quote = () => {
+  const [quote, setQuote] = useState('');
+  const getData = async () => {
+    const data = await getQuote();
+    setQuote(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="quoteContainer">
+      <h3>
+        {`${quote.quote} - ${quote.author}`}
+      </h3>
+    </div>
+  );
+};
 
 export default Quote;
